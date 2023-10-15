f40eafd5eb098d0b9dadee248c6ab6d879f838ec
abbyyT <abitesfahunn@gmail.com>
2023-10-15 18:40:22 +0300

    Create Substring_with_Concatenation_of_All_Words.py

---
+++ Substring_with_Concatenation_of_All_Words.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+        ls = len(s)
+        word_ls = len(words[0])
+        target_dict = {}
+        # create a targe dict for match
+        for word in words:
+            try:
+                target_dict[word] += 1
+            except KeyError:
+                target_dict[word] = 1
+        res = []
+        for start in range(ls - word_ls * len(words) + 1):
+            curr_dict = target_dict.copy()
+            for pos in range(start, start + word_ls * len(words), word_ls):
+                curr = s[pos:pos + word_ls]
+                try:
+                    curr_dict[curr] -= 1
+                    # word appears more than target
+                    if curr_dict[curr] < 0:
+                        break
+                except KeyError:
+                    # word not in words
+                    break
+            else:
+                # all word in target dict
+                res.append(start)
+        return res
