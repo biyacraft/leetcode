87eeac0bc79efd306837846161163b327412406c
abbyyT <abitesfahunn@gmail.com>
2023-10-16 22:04:02 +0300

    Create fizz-buzz.py

---
+++ fizz-buzz.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        dict = []
+        for i in range(1,n+1):
+            if i%3==0 and i%5==0:
+                dict.append("FizzBuzz")
+            elif i%3==0:
+                dict.append("Fizz")
+            elif i%5==0:
+                dict.append("Buzz")
+            else:
+                dict.append(str(i))
+        return dict
