eea1b8e10904171f3125a5f5206fc2ef5fc426b4
abbyyT <abitesfahunn@gmail.com>
2023-12-04 03:26:26 +0300

    Create find-the-k-beauty-of-a-numberr.py.py

---
+++ find-the-k-beauty-of-a-numberr.py.py
@@ -0,0 +1,22 @@
+#find-the-k-beauty-of-a-number
+class Solution(object):
+    def divisorSubstrings(self, num, k):
+        """
+        :type num: int
+        :type k: int
+        :rtype: int
+        """
+        if not num:
+            return None
+        num_str = str(num)
+        n = len(num_str)
+        count = 0
+
+        for i in range(n - k + 1):
+            substring = num_str[i:i + k]
+            if substring[0] != '0':
+                sub_num = int(substring)
+                if sub_num != 0 and num % sub_num == 0:
+                    count += 1
+
+        return count
