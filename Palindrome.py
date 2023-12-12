f6f473795c2b9c35bec2d58d29faf8249502eac9
abbyyT <abitesfahunn@gmail.com>
2023-12-12 14:17:42 -0800

    Create Palindrome.py

---
+++ Palindrome.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        string = ""
+        for char in s:
+            if char.isalnum():
+                string += char.lower()
+        
+        start = 0
+        end = len(string) - 1
+        while start < end:
+            if string[start] != string[end]:
+                return False
+            start += 1
+            end -= 1
+        return True
