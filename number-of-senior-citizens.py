1f9ef0bb366b62d9a1583cd7e182e6dd2a6366c8
ab4io <abitesfahunn@gmail.com>
2024-08-02 01:36:58 +0300

    Create number-of-senior-citizens.py

---
+++ number-of-senior-citizens.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        count = 0
+        for detail in details:
+            age = int(detail[11:13])
+            if age > 60:
+                count += 1
+        return count
